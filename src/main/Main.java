f85aee3d31e117ed3a1a62aaf6e8c8cc243a632b
Rychagov D.I <ruchagov-diman@mail.ru>
2017-02-12 15:44:34 +0400

    Добавил верхнее меню

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,16 +8,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../scheme/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("../scheme/main.fxml"));
+    primaryStage.setTitle("Hello World");
+    primaryStage.setScene(new Scene(root, 800, 600));
+    primaryStage.setResizable(false);
+    primaryStage.show();
+  }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
