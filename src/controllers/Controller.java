c085abb549a24c5cd06de57a50c3d3da4812cdbd
Rychagov D.I <ruchagov-diman@mail.ru>
2017-02-12 17:44:50 +0400

    Добавил обработку клика в режиме ввода вершин

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,10 +1,16 @@
 package controllers;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
+import javafx.scene.Group;
+import javafx.scene.SubScene;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 public class Controller {
 
@@ -16,17 +22,24 @@ public class Controller {
   private RadioButton leftMenuStart;
   @FXML
   private RadioButton leftMenuEnd;
+  @FXML
+  private SubScene drawField;
+
+  private Group drawFieldGroup;
 
   public void initialize() {
-    initToggle();
+    initLeftMenu();
+    initDrawField();
   }
 
-  private void initToggle(){
+  // Инициализирует боковое меню
+  private void initLeftMenu(){
     ToggleGroup toggleGroup = new ToggleGroup(); // Объединяет кнопки в группу
 
     leftMenuDot.setToggleGroup(toggleGroup);
     leftMenuDot.setTooltip(new Tooltip("Режим рисования вершин")); // Подсказка при наведении курсора
     leftMenuDot.setCursor(Cursor.HAND);                                 // Вид курсора
+    leftMenuDot.setSelected(true);
 
     leftMenuLine.setToggleGroup(toggleGroup);
     leftMenuLine.setTooltip(new Tooltip("Режим рисования рёбер"));
@@ -39,5 +52,26 @@ public class Controller {
     leftMenuEnd.setToggleGroup(toggleGroup);
     leftMenuEnd.setTooltip(new Tooltip("Режим выбора конца"));
     leftMenuEnd.setCursor(Cursor.HAND);
+  }
+
+  private void initDrawField() {
+
+    drawFieldGroup = new Group();
+    drawField.setRoot(drawFieldGroup);
+    drawField.setFill(Paint.valueOf("white"));
+
+    drawField.setOnMouseClicked(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent event) {
+
+        // TODO Классы точек, линий и обработка их создания
+
+        // Здесь создается объект Circle и помещается на экран
+        if (leftMenuDot.isSelected()) {
+          drawFieldGroup.getChildren().add(new Circle((int) event.getSceneX() - 55, (int) event.getSceneY() - 25, 5));
+        }
+
+      }
+    });
   }
 }
