416ae392a1a717ff8c5a769af683672579410f35
Rychagov D.I <ruchagov-diman@mail.ru>
2017-02-12 16:42:45 +0400

    Добавил инициализацию бокового меню

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,4 +1,43 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Tooltip;
+
 public class Controller {
+
+  @FXML
+  private RadioButton leftMenuDot;
+  @FXML
+  private RadioButton leftMenuLine;
+  @FXML
+  private RadioButton leftMenuStart;
+  @FXML
+  private RadioButton leftMenuEnd;
+
+  public void initialize() {
+    initToggle();
+  }
+
+  private void initToggle(){
+    ToggleGroup toggleGroup = new ToggleGroup(); // Объединяет кнопки в группу
+
+    leftMenuDot.setToggleGroup(toggleGroup);
+    leftMenuDot.setTooltip(new Tooltip("Режим рисования вершин")); // Подсказка при наведении курсора
+    leftMenuDot.setCursor(Cursor.HAND);                                 // Вид курсора
+
+    leftMenuLine.setToggleGroup(toggleGroup);
+    leftMenuLine.setTooltip(new Tooltip("Режим рисования рёбер"));
+    leftMenuLine.setCursor(Cursor.HAND);
+
+    leftMenuStart.setToggleGroup(toggleGroup);
+    leftMenuStart.setTooltip(new Tooltip("Режим выбора начала"));
+    leftMenuStart.setCursor(Cursor.HAND);
+
+    leftMenuEnd.setToggleGroup(toggleGroup);
+    leftMenuEnd.setTooltip(new Tooltip("Режим выбора конца"));
+    leftMenuEnd.setCursor(Cursor.HAND);
+  }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("../scheme/main.fxml"));
-    primaryStage.setTitle("Hello World");
+    primaryStage.setTitle("Graph builder");
     primaryStage.setScene(new Scene(root, 800, 600));
     primaryStage.setResizable(false);
     primaryStage.show();
