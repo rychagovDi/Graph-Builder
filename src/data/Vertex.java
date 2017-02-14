72ffc4c37c5544935c1e8dca3636df605f9ac08b
Rychagov D.I <ruchagov-diman@mail.ru>
2017-02-14 22:15:35 +0400

    Добавил поле id в класс Vertex. Хранит текуищий номер вершины.

--- src/data/Vertex.java
+++ src/data/Vertex.java
@@ -3,12 +3,15 @@ package data;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 public class Vertex extends Circle{
 
   private int sumWeight;
+  private int _id; // Номер вершины
   private boolean isStart;
   private boolean isEnd;
+  private Text _idField; // Поле для отображения номера вершины
 
   public Vertex(double centerX, double centerY, double radius) {
     super(centerX, centerY, radius);
@@ -17,6 +20,8 @@ public class Vertex extends Circle{
     setStrokeWidth(0);
     isStart = false;
     isEnd = false;
+    _idField = new Text(getCenterX() + 5, getCenterY() - 5, "" + _id);
+    _idField.setFill(Paint.valueOf(Color.BLUEVIOLET.toString()));
   }
 
   public boolean isStart() {
@@ -38,5 +43,17 @@ public class Vertex extends Circle{
   }
   public void setSumWeight(int sumWeight) {
     this.sumWeight = sumWeight;
+  }
+
+  public int get_id() {
+    return _id;
+  }
+  public void set_id(int id) {
+    this._id = id;
+    _idField.setText("" + _id);
+  }
+
+  public Text get_idField() {
+    return _idField;
   }
 }
